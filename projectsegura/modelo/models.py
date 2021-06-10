558755c336a00b4c33d545aebe2fef0695d9eeeb
ibrainRodriguez <42576486+ibrainRodriguez@users.noreply.github.com>
2021-06-09 22:11:51 -0500

    configuracionmodelos

--- projectsegura/modelo/models.py
+++ projectsegura/modelo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class usuarios(models.Model):
+    nombre = models.CharField(max_length=60)
+    usuario = models.CharField(max_length=30)
+    contra = models.CharField(max_length=40)
+    correo = models.EmailField()
